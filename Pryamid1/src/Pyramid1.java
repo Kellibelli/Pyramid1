a9097be724580b04e295e49d79b113a32ef350df
Kelli Garner <32491649+Kellibelli@users.noreply.github.com>
2019-09-17 17:36:07 -0600

    Add files via upload

---
+++ Pryamid1/src/Pyramid1.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Pyramid1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the number of lines: ");
+        int number = input.nextInt();
+
+        if (number >= 10) {
+            for (int i = 1; i <= number; i++) {
+
+                for (int j = 1; j <= number - i; j++) {
+                    System.out.print("    ");
+                }
+                for (int h = i; h >= 1; h--) {
+                    System.out.printf("%4d", h);
+                }
+                for (int k = 2; k <= i; k++) {
+                    System.out.printf("%4d", k);
+
+                }
+
+                System.out.println();
+            }
+        } else {
+            for (int i = 1; i <= number; i++) {
+
+                for (int j = 1; j <= number - i; j++) {
+                    System.out.printf("  ");
+                }
+                for (int h = i; h >= 1; h--) {
+                    System.out.printf("%2d", h);
+                }
+                for (int k = 2; k <= i; k++) {
+                    System.out.printf("%2d", k);
+
+                }
+
+                System.out.println();
+            }
+        }
+
+
+    }
+
+
+}
+
+
+
